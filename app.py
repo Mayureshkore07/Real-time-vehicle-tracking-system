cf89c2acf15ed4c10c0d4bb5ab866e6ff23451fb
Mayureshkore07 <mayureshkore07@gmail.com>
2025-03-29 12:52:45 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -97,10 +97,5 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')  # Make sure index.html exists in 'templates/' folder
 
-if __name__ == '__main__':
-    app.run(debug=True)
 
